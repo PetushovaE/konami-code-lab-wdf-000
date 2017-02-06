1ac88d694fa1c604b7d8c8fe1675421cf215ad3b
Ekaterina Petushova <epetushova@gmail.com>
2017-02-06 18:43:39 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+
  
 // keep track of index outside of the event handler
 var index = 0
-document.body.addEventListener('keydown', function(e){
 
-	const key = parseInt(e.detail || e.which)
+function init() {
+	document.body.addEventListener('keydown', onKeyDownHandler)
+ 
+// unattached event handler
+function onKeyDownHandler(e) {
+  	const key = parseInt(e.detail || e.which)
  
   	if (key === code[index]) {
     	index++
@@ -18,5 +22,29 @@ document.body.addEventListener('keydown', function(e){
   	} else {
     	index = 0
   		}
-	})
+	}
 }
+
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+// function init() {
+ 
+// // keep track of index outside of the event handler
+// var index = 0
+// document.body.addEventListener('keydown', function(e){
+
+// 	const key = parseInt(e.detail || e.which)
+ 
+//   	if (key === code[index]) {
+//     	index++
+ 
+//     if (index === code.length - 1) {
+//       	alert("YOU DID IT!")
+//       	index = 0
+//     	}
+//   	} else {
+//     	index = 0
+//   		}
+// 	})
+// }
+
